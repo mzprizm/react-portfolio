660be59cbf4d5e69d70c76e5db44aa4261905da7
Carolyn Nickell <caro.lovin.it@gmail.com>
2020-04-21 20:02:17 -0700

    updated structure and added styled components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Main from './components/Main';
-import Footer from './components/Footer';
+import Footer from './components/Footer/Footer';
 import Header from './components/Header';
 
 function App() {
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Flex from "./Flex";
+import Flex from "../Flex";
 
 export default function Footer() {
   return (
